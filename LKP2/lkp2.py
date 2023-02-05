b5a063596ec8367c0370c3057341d9a149fc2487
DR34MBoy <fgh0608@gmail.com>
2023-02-05 14:43:51 +0700

    Update lkp2.py

--- LKP2/lkp2.py
+++ LKP2/lkp2.py
@@ -1,24 +1,28 @@
 import numpy as np
 import cv2
 
-def RGB_to_HSV(r, g, b):
+def RGB2HSV(r, g, b):
     r, g, b = r/255.0, g/255.0, b/255.0
-    mx = max(r, g, b)
-    mn = min(r, g, b)
-    df = mx-mn
-    if mx == mn:
+
+    cmx = max(r, g, b)
+    cmn = min(r, g, b)
+    diff = cmx-cmn
+
+    if cmx == cmn:
         h = 0
-    elif mx == r:
-        h = (60 * ((g-b)/df) + 360) % 360
-    elif mx == g:
-        h = (60 * ((b-r)/df) + 120) % 360
-    elif mx == b:
-        h = (60 * ((r-g)/df) + 240) % 360
-    if mx == 0:
+    elif cmx == r:
+        h = (60 * ((g-b)/diff) + 360) % 360
+    elif cmx == g:
+        h = (60 * ((b-r)/diff) + 120) % 360
+    elif cmx == b:
+        h = (60 * ((r-g)/diff) + 240) % 360
+
+    if cmx == 0:
         s = 0
     else:
-        s = df/mx
-    v = mx
+        s = diff/cmx
+        
+    v = cmx
     return h, s, v
 
 def convert_image(image_path):
@@ -27,11 +31,47 @@ def convert_image(image_path):
     for x in range(col):
         for y in range(row):
             b, g, r = image[y, x]
-            h, s, v = RGB_to_HSV(r, g, b)
+            h, s, v = RGB2HSV(r, g, b)
             image[y, x] = (int(h/2), int(s*255), int(v*255))
     return image
 
+def avg_int(image):
+    total = 0
+    counter = 0
+    row, col = image.shape
+    for x in range(col):
+        for y in range(row):
+            total += image[x,y]
+            counter += 1
+    avg = total/counter
+    return avg
 
-test = convert_image("lenna.png")
-cv2.imshow(test)
+def change_px(image):
+    avg = avg_int(image)
+    row, col = image.shape
+    for x in range(col):
+        for y in range(row):
+            if image[x,y] < avg:
+                image[x,y] = 0
+            else:
+                image[x,y] = 255
+    return image
+
+img = convert_image("lenna.png")
+cv2.imshow("test1", img)
+
+h,s,v = cv2.split(img)
+
+print(avg_int(h))
+print(avg_int(s))
+print(avg_int(v))
+
+test_h = change_px(h)
+test_s = change_px(s)
+test_v = change_px(v)
+
+cv2.imshow("h", test_h)
+cv2.imshow("s", test_s)
+cv2.imshow("v", test_v)
+
 cv2.waitKey(0)
