2a00424c30b57c588aef11ba0028650f76353545
DR34MBoy <fgh0608@gmail.com>
2023-02-13 06:47:33 +0700

    Update lkp3.py

--- LKP3/lkp3.py
+++ LKP3/lkp3.py
@@ -31,27 +31,68 @@ def contrastStretch(image):
 def histogram(image):
     img = np.asarray(image)
     flat = img.flatten()
-    plt.hist(flat, bins = 255)
+
+    plt.hist(flat, bins = 256)
     plt.show()
 
-## Masih Belum Bener
 def norm_hist(image):
-    img = np.asarray(image)
-    flat = img.flatten()
-    plt.hist(flat, bins=255, density=True, color='blue', alpha=0.7, rwidth=0.85)
+    flat = image.flatten()
+
+    histogram, _ = np.histogram(flat, bins=256, range=(0, 255), density=True)
+
+    normalized_histogram = histogram / histogram.sum()
+
+    plt.bar(np.arange(256), normalized_histogram)
+    plt.xlim([0, 256])
+    plt.xlabel("Pixel Intensity")
+    plt.ylabel("Frequency")
     plt.show()
 
 def cmltv_histogram(image):
-    img = np.asarray(image)
-    flat = img.flatten()
-    plt.hist(flat, bins = 255, cumulative = True)
+    flat = image.flatten()
+    
+    histogram, _ = np.histogram(flat, bins=256, range=(0, 255), density=True)
+    
+    cumulative_histogram = np.zeros(256)
+    cumulative_histogram[0] = histogram[0]
+
+    for i in range(1, 256):
+        cumulative_histogram[i] = cumulative_histogram[i-1] + histogram[i]
+    
+    plt.bar(np.arange(256), cumulative_histogram)
+    plt.xlim([0, 256])
+    plt.xlabel("Pixel Intensity")
+    plt.ylabel("Frequency")
     plt.show()
 
-# Jangan pakai fungsi ini
+# Masih salah
+def equal_hist(image):
+    flat_image = image.flatten()
+    histogram, bins = np.histogram(flat_image, bins=256, range=(0, 255))
+
+    cumulative_histogram = np.cumsum(histogram)
+    cumulative_histogram = cumulative_histogram / cumulative_histogram[-1]
+    
+    equalized = np.interp(flat_image, bins[:-1], cumulative_histogram)
+    eqlz = equalized.reshape(image.shape)
+
+    plt.hist(eqlz.flatten(), bins=256, range=(0, 255))
+    plt.xlim([0, 256])
+    plt.xlabel("Pixel Intensity")
+    plt.ylabel("Frequency")
+    plt.show()
+
+# Jangan pakai fungsi di bawah ini
 def equ(image):
     # masih pake fungsi dl
     img_equ = cv2.equalizeHist(image)
     return img_equ
+
+def cmltv_histogram2(image):
+    img = np.asarray(image)
+    flat = img.flatten()
+    plt.hist(flat, bins = 256, cumulative = True)
+    plt.show()
 
 
 
@@ -75,11 +116,12 @@ equ_stretch = equ(stretch)
 # cv2.imshow("gray", gray_melon) 
 # cv2.imshow("stretch", stretch) 
 
-histogram(gray_melon) 
-norm_hist(stretch) 
+# histogram(gray_melon) 
+# norm_hist(gray_melon) 
+# cmltv_histogram2(stretch)
+equal_hist(gray_melon)
 
 
-# norm_hist(gray_melon)
 
 
 cv2.waitKey(0)
