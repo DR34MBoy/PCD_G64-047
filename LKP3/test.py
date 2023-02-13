cbecb0e8423bdd5e2715b16405a8747c458f738e
DR34MBoy <fgh0608@gmail.com>
2023-02-13 17:18:48 +0700

    lkp3

--- LKP3/test.py
+++ LKP3/test.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-import PyQt5
 from numpy import *
 
 def convert_gray(image):
@@ -28,6 +27,10 @@ def contrastStretch(image):
 
     return ctrStretch
 
+
+
+
+# Membuat Histogram
 def histogram(image):
     img = np.asarray(image)
     flat = img.flatten()
@@ -43,64 +46,43 @@ def norm_hist(image):
 
     return normalized_histogram
 
-def cmltv_histogram(image):
-    flat = image.flatten()
+def cmltv_hist(image):
+    histogram = np.zeros(256, dtype=int)
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            histogram[image[i, j]] += 1
     
-    histogram, _ = np.histogram(flat, bins=256, range=(0, 255), density=True)
-    
-    cumulative_histogram = np.zeros(256)
-    cumulative_histogram[0] = histogram[0]
-
+    cdf = np.zeros(256, dtype=int)
+    cdf[0] = histogram[0]
     for i in range(1, 256):
-        cumulative_histogram[i] = cumulative_histogram[i-1] + histogram[i]
+        cdf[i] = cdf[i - 1] + histogram[i]
     
-    return cumulative_histogram
+    return cdf
 
-# Masih salah
-def equal_hist(image, cdf):
-    # Convert the image to grayscale
-    grayscale_image = image
+def equal_hist(image):
+    cdf = cmltv_hist(image)
+    
+    cdf_min = np.min(cdf)
+    cdf_max = np.max(cdf)
+    cdf_normalized = (cdf - cdf_min) / (cdf_max - cdf_min) * 255
+    
+    equalized_image = np.zeros(image.shape, dtype=np.uint8)
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            equalized_image[i, j] = cdf_normalized[image[i, j]].astype(np.uint8)
 
-    
-    # # Calculate the histogram of the grayscale image
-    # histogram = np.zeros(256, dtype=np.int)
-    # for i in range(grayscale_image.shape[0]):
-    #     for j in range(grayscale_image.shape[1]):
-    #         histogram[grayscale_image[i, j]] += 1
-    
-    # # Calculate the cumulative distribution function (CDF) of the histogram
-    # cdf = np.zeros(256, dtype=np.int)
-    # cdf[0] = histogram[0]
-    # for i in range(1, 256):
-    #     cdf[i] = cdf[i - 1] + histogram[i]
-    
-    # Normalize the CDF
-
-    cdf_normalized = cdf
-    
-    # Map the intensity values of the grayscale image to the normalized CDF
-    equalized_image = np.zeros(grayscale_image.shape, dtype=np.uint8)
-    for i in range(grayscale_image.shape[0]):
-        for j in range(grayscale_image.shape[1]):
-            equalized_image[i, j] = cdf_normalized[grayscale_image[i, j]].astype(np.uint8)
-    
     return equalized_image
 
-# Jangan pakai fungsi di bawah ini
-def equ(image):
-    # masih pake fungsi dl
-    img_equ = cv2.equalizeHist(image)
-    return img_equ
 
-def cmltv_histogram2(image):
-    img = np.asarray(image)
-    flat = img.flatten()
-    plt.hist(flat, bins = 256, cumulative = True)
-    plt.show()
 
+
+# Menamplikan Histogram
 def draw_hist(image, name):
-    if name == "Histogram" :
-        plt.hist(image, bins = 256)
+    if name == "Histogram" or name == "Equalize" :
+        plt.hist(image.flatten(), bins=256, range=(0, 255))
+        plt.xlim([0, 256])
+        plt.xlabel("Pixel Intensity")
+        plt.ylabel("Frequency")
         plt.show()
     elif name == "Normalize" or name == "Cumulative" :
         plt.bar(np.arange(256), image)
@@ -108,50 +90,25 @@ def draw_hist(image, name):
         plt.xlabel("Pixel Intensity")
         plt.ylabel("Frequency")
         plt.show()
-    elif name == "Equalize" :
-        plt.hist(image.flatten(), bins=256, range=(0, 255))
-        plt.xlim([0, 256])
-        plt.xlabel("Pixel Intensity")
-        plt.ylabel("Frequency")
-        plt.show()
-
-
-
 
 
 
 
 # Main Driver
-
 melon = cv2.imread("melon.jpeg")
-
 gray_melon = convert_gray(melon)
-
 stretch = contrastStretch(gray_melon)
+equal = equal_hist(gray_melon)
 
-equ_stretch = equ(stretch)
+cv2.imshow("ori", melon)
+cv2.imshow("gray", gray_melon)
+cv2.imshow("ekualisasi", equal) 
+cv2.imshow("stretch", stretch) 
 
-
-
-# cv2.imshow("melon", melon)
-# cv2.imshow("gray", gray_melon) 
-# cv2.imshow("stretch", stretch) 
-
-# histogram(gray_melon) 
-# norm_hist(gray_melon) 
-
-# draw_hist(histogram(gray_melon), "Histogram")
-# draw_hist(norm_hist(gray_melon), "Normalize")
-# draw_hist(cmltv_histogram(stretch), "Cumulative")
-
-
-draw_hist(equal_hist(gray_melon, cmltv_histogram(stretch)), "Equalize")
-
-
-# cmltv_histogram2(stretch)
-# equal_hist(melon)
-
-
-
+draw_hist(histogram(gray_melon),"Histogram")
+draw_hist(histogram(stretch),"Histogram")
+draw_hist(norm_hist(gray_melon),"Normalize")
+draw_hist(cmltv_hist(gray_melon),"Cumulative")
+draw_hist(equal_hist(gray_melon),"Equalize")
 
 cv2.waitKey(0)
