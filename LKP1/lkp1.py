07f2a2863c700ed3019f49ccfbe64a1f91590ca3
DR34MBoy <fgh0608@gmail.com>
2023-02-14 10:44:43 +0700

    Update lkp1.py

--- LKP1/lkp1.py
+++ LKP1/lkp1.py
@@ -1,38 +1,51 @@
 import numpy as np
 import cv2
 
+# Fungsi utama
+# Fungsi untuk mendapatkan matrix dari suatu gambar masukan menggunakan fungsi np.array()
+def matrix_img(image):
+    matrix = np.array(image)
+    return matrix
+
+# Fungsi untuk mengubah intensitas pixel gambar dengan threshold tertentu
 def change_px (image):
+    row, col ,ch = img_daun.shape
     for i in range(0,row):
         for j in range(0,col):
             if image[i, j] < 150:
                 image[i, j] = 255
-            else:
-                image[i, j] = image[i, j]
     return image
 
+# Fungsi untuk mendapatkan transpose dari matrix menggunakan fungsi transpose()
+def transp_matrix(matrix):
+    tp = matrix.transpose()
+    return tp
+
+
+
+
+# Main Driver
+# Membaca gambar daun lalu menampilkannya
 img_daun = cv2.imread("daun.jpg")
-row, col ,ch = img_daun.shape
-
-b, g, r = cv2.split(img_daun)
-
 cv2.imshow("daun", img_daun)
 
-# cv2.imshow("b", b)
-# cv2.imshow("g", g)
-# cv2.imshow("r", r)
+# Menggunakan fungsi matrix_img untuk mendapatkan matrix dari gambar daun, lalu menampilkannya
+matrix = matrix_img(img_daun)
+print(matrix)
 
+# Memecah tiap channel menggunakan fungsi split
+b, g, r = cv2.split(img_daun)
+# Lalu melakukan perubahan intensitas pixel dengan fungsi change_px sesuai dengan threshold
 change_b = change_px(b)
 change_g = change_px(g)
 change_r = change_px(r)
-
-# cv2.imshow("b", change_b)
-# cv2.imshow("g", change_g)
-# cv2.imshow("r", change_r)
-
+# Menggabungkan kembali channel yang telah dipisah dan diubah
 merge = cv2.merge([change_b, change_g, change_r])
-
+# Menampilkan gambar setelah digabung
 cv2.imshow("merge", merge)
 
+# Menggunakan fungsi transp_matrix untuk mendapatkan transpose dari matrix, lalu menampilkannya
+transpose = transp_matrix(matrix)
+print(transpose)
 
 cv2.waitKey(0)
-
