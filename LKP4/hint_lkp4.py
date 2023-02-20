57f564870952d9ea3c7d6bece52ab4e79f909eea
DR34MBoy <fgh0608@gmail.com>
2023-02-20 17:21:17 +0700

    Update hint_lkp4.py

--- LKP4/hint_lkp4.py
+++ LKP4/hint_lkp4.py
@@ -1,29 +1,38 @@
-# Import libraries
+# Imoort library yang akan digunakan
 import numpy as np
 import cv2
 
-# Define convolution progam
+# Fungsi Konvolusi
 def customConvolution(img, mask):
-   row, col, ch= img.shape
-   rowMask, colMask = len(mask), len(mask)
-   canvas = np.zeros((row, col, 1), dtype="uint8")
-   for i in range(0,row-1):
-       for j in range(0,col-1):
-           imgSum = 0; maskSum = 0
-           for a in range(-int(rowMask/2) , rowMask - int(rowMask/2)):
-               for b in range(-int(colMask/2), colMask - int(colMask/2)):
-                   if (i + a >= 0) and (j + b >= 0):
-                       imgSum = imgSum + img[i+a, j+b][0] * mask[a + int(rowMask/2)][b + int(colMask/2)]
-                       maskSum = maskSum + mask[a + int(rowMask/2)][b + int(colMask/2)]
-           intensity = imgSum / maskSum
-           if intensity > 255:
-               intensity = 255
-           elif intensity < 0:
-               intensity = 0
-           canvas[i, j] = intensity
-   return canvas
+    # Mendapatkan baris, kolom, dan channel gambar masukan
+    row, col, ch= img.shape
 
-# Convert image to grayscale
+    # Mendapatkan baris dan kolom dari mask / filter
+    rowMask, colMask = len(mask), len(mask)
+
+    # Mendapatkan matriks gambar bernama canvas
+    canvas = np.zeros((row, col, 1), dtype="uint8")
+
+    # Proses konvolusi
+    for i in range(0,row-1):
+        for j in range(0,col-1):
+            imgSum = 0; maskSum = 0
+            for a in range(-int(rowMask/2) , rowMask - int(rowMask/2)):
+                for b in range(-int(colMask/2), colMask - int(colMask/2)):
+                    if (i + a >= 0) and (j + b >= 0):
+                        imgSum = imgSum + img[i+a, j+b][0] * mask[a + int(rowMask/2)][b + int(colMask/2)]
+                        maskSum = maskSum + mask[a + int(rowMask/2)][b + int(colMask/2)]
+            intensity = imgSum / maskSum
+            if intensity > 255:
+                intensity = 255
+            elif intensity < 0:
+                intensity = 0
+            canvas[i, j] = intensity
+
+    # Mengembalikan matriks gambar "canvas"
+    return canvas
+
+# Fungsi mengubah gambar menjadi grayscale
 def convert_gray(image):
     # Mendapatkan baris, kolom, dan channel gambar masukan
     row, col, ch = image.shape
@@ -40,10 +49,10 @@ def convert_gray(image):
     return gray
 
 
-# Read image
+# Membaca gambar
 img = cv2.imread("lenna.png")
 
-# Create pass filter kernels
+# Membuat filter kernel
 lowPassFilter = np.array([[1/9, 1/9, 1/9],
                           [1/9, 1/9, 1/9],
                           [1/9, 1/9, 1/9]])
@@ -54,12 +63,15 @@ directPassFilter = np.array([[-1, 0, 1],
                              [-1, 0, 1],
                              [-1, 0, 1]])
 
+# Mengubah gambar menjadi grayscale
 canvas = convert_gray(img)
 
+# Mendapatkan gambar dengan metode konvolusi
 result_lpf = customConvolution(canvas, lowPassFilter)
 result_hpf = customConvolution(canvas, highPassFilter)
 result_dpf = customConvolution(canvas, directPassFilter)
 
+# Menampilkan gambar hasil proses konvolusi
 cv2.imshow("Low pass filter image", result_lpf)
 cv2.imshow("High pass filter image", result_hpf)
 cv2.imshow("Direct pass filter image", result_dpf)
